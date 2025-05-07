422f81b91394d5ae2db4aeec3fb50dc3f1214390
Pranava Bhat <72594385+pranavabhat-5204@users.noreply.github.com>
2025-05-07 10:21:03 +0530

    Update script.py

--- script.py
+++ script.py
@@ -5,7 +5,6 @@ from skimage import io
 from matplotlib import pyplot as plt
 
 
-
 img_flag = cv2.imread('/content/flag.png')  # Destination image
 img_pattern = cv2.imread('/content/pattern.png') # Source image
 
@@ -16,9 +15,9 @@ plt.imshow(img_pattern)
 sor_pts = np.float32([[0, 0], [img_pattern.shape[1], 0], [img_pattern.shape[1], img_pattern.shape[0]], [0, img_pattern.shape[0]]])
 dest_pts = np.float32([[300, 70], [1035, 250], [1000, 800], [300, 700]])
 
-H, mask = cv2.findHomography(sor_pts, dest_pts, cv2.RANSAC, 5.0)
+matrix = cv2.getPerspectiveTransform(sor_pts, dest_pts)
 
-warped_pattern = cv2.warpPerspective(img_pattern, H, (img_flag.shape[1], img_flag.shape[0]))
+warped_pattern = cv2.warpPerspective(img_pattern, matrix, (img_flag.shape[1], img_flag.shape[0]))
 
 gray_warped_pattern = cv2.cvtColor(warped_pattern, cv2.COLOR_BGR2GRAY)
 _, mask = cv2.threshold(gray_warped_pattern, 1, 255, cv2.THRESH_BINARY)
