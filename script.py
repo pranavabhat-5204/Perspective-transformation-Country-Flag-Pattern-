6f8bff1bacc57febd1a2a231aabf99fa7db14824
Pranava Bhat <72594385+pranavabhat-5204@users.noreply.github.com>
2025-05-07 22:23:08 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,28 +1,32 @@
+#importing the libraries
 import numpy as np
-import streamlit as st
 import cv2
 from skimage import io
 from matplotlib import pyplot as plt
 
 
-img_flag = cv2.imread('/content/flag.png')  # Destination image
-img_pattern = cv2.imread('/content/pattern.png') # Source image
+img_flag = cv2.imread('/content/flag.png')  # image to which the patter has to be transformed
+img_pattern = cv2.imread('/content/pattern.png') # pattern image
 
+#finding the coordinates of the images from the plot
 plt.imshow(img_flag)
 plt.imshow(img_pattern)
 
-
+#including the coordinates which we have found out
 sor_pts = np.float32([[0, 0], [img_pattern.shape[1], 0], [img_pattern.shape[1], img_pattern.shape[0]], [0, img_pattern.shape[0]]])
 dest_pts = np.float32([[300, 70], [1035, 250], [1000, 800], [300, 700]])
 
+#getting the matrix
 matrix = cv2.getPerspectiveTransform(sor_pts, dest_pts)
 
+#here I used the grey wrapping to improve the isolation for the flag from the background
 warped_pattern = cv2.warpPerspective(img_pattern, matrix, (img_flag.shape[1], img_flag.shape[0]))
 gray_warped_pattern = cv2.cvtColor(warped_pattern, cv2.COLOR_BGR2GRAY)
 _, mask = cv2.threshold(gray_warped_pattern, 1, 255, cv2.THRESH_BINARY)
 pattern = cv2.bitwise_and(warped_pattern, warped_pattern, mask=mask)
 flag = cv2.bitwise_and(img_flag, img_flag, mask=cv2.bitwise_not(mask))
 
+#getting the resulting image
 result = cv2.add(pattern, flag)
 result = cv2.add(pattern, flag)
 result_color = cv2.cvtColor(result, cv2.COLOR_BGR2RGB)
